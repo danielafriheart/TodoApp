d77d7ee6527306a8a20f1c4d91cdc8bd73bad2dd
Daniel Esuola <esuoladaniel002@gmail.com>
2022-10-20 15:45:25 +0100

    getting information from the creat budget page

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget.component.css']
 })
 export class BudgetComponent implements OnInit {
+  // Input variables
+  public budgetName = "";
+  public budgetAmount = "";
+  public budgetNote = "";
+  public budgetTarget = "";
+
+  public budgetArray: any = [];// Array
+
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  createBudget() {
+    // alert("Gbemidebe!!")
+    var todoObj = {
+      budgetName: this.budgetName,
+      budegtAmount: this.budgetAmount,
+      budgetNote: this.budgetNote,
+      budgetTarget: this.budgetTarget
+    }
+    this.budgetArray.push(todoObj)
+    console.log(this.budgetArray);
+
   }
 
 }
